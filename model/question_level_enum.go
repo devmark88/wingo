55942541eae4822392ab3fbb889df9c7ee7aaeb0
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-07 04:32:04 +0330

    add level enum

---
+++ model/question_level_enum.go
@@ -0,0 +1,24 @@
+package model
+
+import (
+	"database/sql/driver"
+)
+
+type QuestionLevelEnum string
+
+const (
+	Mindless QuestionLevelEnum = "mindless"
+	Easy     QuestionLevelEnum = "easy"
+	Medium   QuestionLevelEnum = "medium"
+	Hard     QuestionLevelEnum = "hard"
+	Cruel    QuestionLevelEnum = "cruel"
+)
+
+func (e *QuestionLevelEnum) Scan(value interface{}) error {
+	*e = QuestionLevelEnum(value.([]byte))
+	return nil
+}
+
+func (e QuestionLevelEnum) Value() (driver.Value, error) {
+	return string(e), nil
+}
