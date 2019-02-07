4f62c8728bdff6dcb1d21b76824be220af492a0f
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-07 04:32:55 +0330

    add postgres connector

--- connectors/pg.go
+++ connectors/pg.go
@@ -1,33 +1,39 @@
 package connectors
 
 import (
-	"database/sql"
 	"fmt"
 
-	"gitlab.com/mt-api/wingo/utils"
+	"gitlab.com/mt-api/wingo/model"
 
-	_ "github.com/lib/pq"
+	"gitlab.com/mt-api/wingo/logger"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 
 	"github.com/spf13/viper"
 )
 
 // ConnectDatabase => Connect to Postgres Database
-func ConnectDatabase() *sql.DB {
-	h := viper.GetString("Database.host")
+func ConnectDatabase() *gorm.DB {
+	h := viper.GetString("database.host")
 	p := viper.GetInt("database.port")
 	pwd := viper.GetString("database.password")
 	u := viper.GetString("database.user")
-	db := viper.GetString("database.name")
+	dbn := viper.GetString("database.dbname")
+	ssl := viper.GetString("database.ssl")
 
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%v/%s?sslmode=disable",
-		u, pwd, h, p, db)
-	pdb, err := sql.Open("postgres", connStr)
-	utils.Debug("Connecting to " + connStr)
-	utils.CheckOrFatal(err)
-	defer pdb.Close()
+	connStr := fmt.Sprintf("host=%s port=%v user=%s dbname=%s password=%s sslmode=%s",
+		h, p, u, dbn, pwd, ssl)
+	logger.Debug("connecting to " + connStr)
+	db, err := gorm.Open("postgres", connStr)
+	logger.CheckOrFatal(err)
+	logger.Debug("pinging database")
+	err = db.DB().Ping()
+	logger.CheckOrFatal(err)
+	logger.Debug("connected to postgres")
+	defer db.Close()
+	db.AutoMigrate(&model.ContestMeta{}, &model.Contest{}, &model.Question{}, &model.UserInfo{}, &model.UserTrack{})
+	logger.Debug("migrated")
 
-	err = pdb.Ping()
-	utils.CheckOrFatal(err)
-	utils.Debug("Connected to PSQL => " + connStr)
-	return pdb
+	return db
 }
