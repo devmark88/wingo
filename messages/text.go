eb8843982c870edea3d0582f295f04629f5870b7
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-08 02:13:36 +0330

    add messages

---
+++ messages/text.go
@@ -0,0 +1,8 @@
+package messages
+
+const (
+	INVALID_CONTEST_TIME = "We have a contest in this range. ID: %v"
+	GENERAL_DB_ERROR     = "Error while working with database: %v"
+	MAPPING_ERROR        = "error in map %s to %s: %v"
+	META_HAS_CONTEST     = "a contest already added to this meta. contestID: %v"
+)
