b223e6e740b6555661471d5d0b39666eb17615a4
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-09 02:50:22 +0330

    add not found message

--- messages/text.go
+++ messages/text.go
@@ -5,4 +5,5 @@ const (
 	GENERAL_DB_ERROR     = "Error while working with database: %v"
 	MAPPING_ERROR        = "error in map %s to %s: %v"
 	META_HAS_CONTEST     = "a contest already added to this meta. contestID: %v"
+	NOT_FOUND            = "%s not found with %s:%v"
 )
