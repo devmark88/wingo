985b944d0075122efdd1c0e9c604dde2d90f8b01
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-08 05:01:52 +0330

    init main repo

--- repository/repository.go
+++ repository/repository.go
@@ -14,6 +14,11 @@ func (cn *Connections) AddMeta(m *model.ContestMeta) error {
 	r := contest.MetaRepository{}
 	return r.SaveMeta(m, cn.DB)
 }
+
+func (cn *Connections) GetMeta(force bool) (error, []*model.ContestMeta) {
+	r := contest.MetaRepository{}
+	return r.GetTodayMeta(cn.DB, force, 3)
+}
 func (cn *Connections) AddContest(m *model.Contest) error {
 	r := contest.QuestionRepository{}
 	return r.SaveContest(m, cn.DB)
