701d1d20a85f8705951d4bc6a64621427bfdc9d0
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-08 05:02:32 +0330

    add reverse function to array helper

--- helpers/array.go
+++ helpers/array.go
@@ -6,3 +6,10 @@ import "strings"
 func ByteArrayToString(a []byte, delim string) string {
 	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
 }
+func ReverseArray(data []*interface{}) {
+	d := make([]*interface{}, len(data))
+	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
+		d[i], d[j] = data[j], data[i]
+	}
+	data = d
+}
