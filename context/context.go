2b2ba509ec45db9bd2640d5dad5c339161cc9710
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-08 21:35:52 +0330

    add claims struct to context

--- context/context.go
+++ context/context.go
@@ -1,7 +1,20 @@
 package context
 
-import "gitlab.com/mt-api/wingo/connectors"
+import (
+	"crypto/rsa"
 
+	"gitlab.com/mt-api/wingo/connectors"
+)
+
+type AuthUser struct {
+	ID     string `json:"id"`
+	Avatar string `json:"avatar"`
+	Cellphone string `json:"cellphone"`
+	Username string `json:"username"`
+}
 type AppContext struct {
 	Connections *connectors.Connections
+	AuthUser    AuthUser
+	AdminKey    *rsa.PublicKey
+	UserKey     *rsa.PublicKey
 }
