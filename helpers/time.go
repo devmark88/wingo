63a75662ef7719f891d2f634590b3d4507873cda
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-09 02:48:52 +0330

    add get time helper

--- helpers/time.go
+++ helpers/time.go
@@ -9,3 +9,12 @@ func TimeInTehran(t time.Time) time.Time {
 	t = t.In(loc)
 	return t
 }
+func GetTime(t time.Time) (hour int, minute int, second int) {
+	layout := "2006-01-02 15:04:05"
+	timeStampString := t.Format(layout)
+	timeStamp, err := time.Parse(layout, timeStampString)
+	if err != nil {
+		return 0, 0, 0
+	}
+	return timeStamp.Clock()
+}
