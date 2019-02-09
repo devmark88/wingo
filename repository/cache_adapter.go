ee1dfeaf731ed2f34532fd39c29003ef13d95cc4
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-09 13:17:08 +0330

    add base method for cache adapter

--- repository/cache_adapter.go
+++ repository/cache_adapter.go
@@ -1,11 +1,28 @@
 package repository
 
 import (
+	"fmt"
+	"time"
+
+	"gitlab.com/mt-api/wingo/helpers"
+
+	"github.com/go-redis/redis"
+
 	"gitlab.com/mt-api/wingo/model"
 )
 
-type CacheAdapter struct{}
+type CacheAdapter struct {
+	Connection *redis.Client
+}
 
 func (c *CacheAdapter) GetUserInfo(id string) *model.UserInfo {
 	return nil
+}
+func (c *CacheAdapter) ContestMeta() []*model.ContestMeta {
+	return nil
+}
+
+func getDateForKey(t time.Time) string {
+	t = helpers.TimeInTehran(t)
+	return fmt.Sprintf("%v-%v-%v", t.Year(), int(t.Month()), t.Day())
 }
