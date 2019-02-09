381b121155e350f1fb61dc1b6dc0a603ebd5800e
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-09 13:17:45 +0330

    call cache adapter methods in repository

--- repository/repository.go
+++ repository/repository.go
@@ -1,24 +1,45 @@
 package repository
 
 import (
+	"fmt"
+
+	"github.com/go-redis/redis"
 	"github.com/jinzhu/gorm"
+	"gitlab.com/mt-api/wingo/logger"
 	"gitlab.com/mt-api/wingo/model"
 	"gitlab.com/mt-api/wingo/repository/contest"
 	"gitlab.com/mt-api/wingo/repository/user"
 )
 
 type Connections struct {
-	DB *gorm.DB
+	DB    *gorm.DB
+	Redis *redis.Client
 }
 
 func (cn *Connections) AddMeta(m *model.ContestMeta) error {
 	r := contest.MetaRepository{}
-	return r.SaveMeta(m, cn.DB)
+	err := r.SaveMeta(m, cn.DB)
+	if err == nil {
+		c := CacheAdapter{Connection: cn.Redis}
+		er := c.InvalidateContestMeta()
+		if er != nil {
+			logger.Error(fmt.Errorf("error while invalidating meta into redis: %v", er))
+		}
+	}
+	return err
 }
 
 func (cn *Connections) GetMeta(force bool) (error, []*model.ContestMeta) {
 	r := contest.MetaRepository{}
-	return r.GetTodayMeta(cn.DB, force, 3)
+	err, t := r.GetTodayMeta(cn.DB, force, 3)
+	if err == nil {
+		c := CacheAdapter{Connection: cn.Redis}
+		er := c.SetContestMeta(t)
+		if er != nil {
+			logger.Error(fmt.Errorf("error while saving meta into redis: %v", er))
+		}
+	}
+	return err, t
 }
 func (cn *Connections) AddContest(m *model.Contest) error {
 	r := contest.QuestionRepository{}
