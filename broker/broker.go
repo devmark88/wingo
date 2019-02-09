2ac30a2d1ea9f28210b6e2cc3eaeb1cd059a9561
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-10 00:59:22 +0330

    fix broker interface

--- broker/broker.go
+++ broker/broker.go
@@ -1,5 +1,15 @@
 package broker
 
+import (
+	"gitlab.com/mt-api/wingo/broker/emq"
+)
+
 type MessageBroker interface {
-	DelayPublish(t string, d int)
+	DelayPublish(t string, d int, payload interface{}) error
+}
+type Broker struct{}
+
+func (b Broker) DelayPublish(t string, d int, payload interface{}) error {
+	e := emq.Broker{}
+	return e.DelayPublish(t, d, payload)
 }
