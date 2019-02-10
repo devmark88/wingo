18ff6555926443eddb7247e391cd866dfedc5639
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-11 03:00:43 +0330

    add worker server

--- utils/server.go
+++ utils/server.go
@@ -3,11 +3,14 @@ package utils
 import (
 	"io/ioutil"
 
+	"gitlab.com/mt-api/wingo/q"
+
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"gitlab.com/mt-api/wingo/connectors"
 	"gitlab.com/mt-api/wingo/context"
+
 	"gitlab.com/mt-api/wingo/handlers"
 	"gitlab.com/mt-api/wingo/logger"
 )
@@ -17,6 +20,10 @@ func Start(r *gin.Engine, cn *connectors.Connections) {
 	ctx := context.AppContext{
 		Connections: cn,
 		AuthUser:    context.AuthUser{},
+	}
+	ctx.Q = &context.QContext{
+		Server:  nil,
+		Workers: nil,
 	}
 	kp := viper.GetString("secrets.admin")
 	s, e := ioutil.ReadFile(kp)
@@ -35,5 +42,13 @@ func Start(r *gin.Engine, cn *connectors.Connections) {
 	handlers.Setup(r, &ctx)
 	p := viper.Get("server.address").(string)
 	logger.Debug("server running on " + p)
+
+	srv, wrk, err := q.Start(1)
+	if err != nil {
+		logger.CheckOrFatal(err)
+	}
+	ctx.Q.Server = srv
+	ctx.Q.Workers = wrk
+
 	r.Run(p)
 }
