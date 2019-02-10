74dd2751bf94880a4b8443d3ce5ef758553f3ba3
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-11 03:02:39 +0330

    add new publish to broker interface

--- broker/broker.go
+++ broker/broker.go
@@ -8,6 +8,7 @@ import (
 
 type MessageBroker interface {
 	DelayPublish(t string, d int, payload interface{}) (interface{}, error)
+	Publish(t string, payload string) (interface{}, error)
 }
 type Broker struct{}
 
@@ -15,6 +16,15 @@ func (b Broker) DelayPublish(t string, d int, payload interface{}) (interface{},
 	client := http.Client{}
 	e := emq.Broker{Client: &client}
 	res, err := e.DelayPublish(t, d, payload)
+	if err != nil {
+		return nil, err
+	}
+	return res.StatusCode, nil
+}
+func (b Broker) Publish(t string, payload string) (interface{}, error) {
+	client := http.Client{}
+	e := emq.Broker{Client: &client}
+	res, err := e.Publish(t, payload)
 	if err != nil {
 		return nil, err
 	}
