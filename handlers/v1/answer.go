13bcca3f68645d34cd847f6b520f06def5a86a50
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-13 00:35:23 +0330

    init answer handler

--- handlers/v1/answer.go
+++ handlers/v1/answer.go
@@ -1,1 +1,25 @@
 package v1
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gitlab.com/mt-api/wingo/repository"
+	"gitlab.com/mt-api/wingo/request"
+)
+
+// PostAnswer : an answer posted by clients
+func (h *Handlers) PostAnswer(c *gin.Context) {
+	var m request.PostAnswer
+	r := repository.Connections{DB: h.Context.Connections.Database, Redis: h.Context.Connections.Cache, Queue: h.Context.Q.Server}
+	c.Header("Content-Type", "application/json; charset=utf-8")
+	if err := c.Bind(&m); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  err.Error(),
+			"status": http.StatusBadRequest,
+		})
+		return
+	}
+	uid := h.Context.AuthUser.ID
+	
+}
