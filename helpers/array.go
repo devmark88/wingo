0ce46e61cc770c9e11a18f81e07aae7b0178770b
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-15 02:53:31 +0330

    add string to array int helper

--- helpers/array.go
+++ helpers/array.go
@@ -1,9 +1,26 @@
 package helpers
 
-import "fmt"
-import "strings"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 // ByteArrayToString : Convert a []byte array to string with delimator
 func ByteArrayToString(a []byte, delim string) string {
 	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+}
+
+// StringToIntArray : Convert a []byte array to string with delimator
+func StringToIntArray(a string, delim string) ([]int, error) {
+	var m []int
+	s := strings.Split(a, delim)
+	for _, r := range s {
+		i, err := strconv.Atoi(r)
+		if err != nil {
+			return nil, err
+		}
+		m = append(m, i)
+	}
+	return m, nil
 }
