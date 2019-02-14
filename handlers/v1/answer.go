6196afb740370db15370dcf1fd4d39e2711c7718
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-15 02:57:37 +0330

    enhance set answer logic

--- handlers/v1/answer.go
+++ handlers/v1/answer.go
@@ -3,6 +3,8 @@ package v1
 import (
 	"net/http"
 
+	"gitlab.com/mt-api/wingo/helpers"
+
 	"gitlab.com/mt-api/wingo/model"
 
 	"github.com/gin-gonic/gin"
@@ -14,7 +16,6 @@ import (
 func (h *Handlers) PostAnswer(c *gin.Context) {
 	var m request.PostAnswer
 	r := repository.Connections{DB: h.Context.Connections.Database, Redis: h.Context.Connections.Cache, Queue: h.Context.Q.Server}
-
 	c.Header("Content-Type", "application/json; charset=utf-8")
 	if err := c.Bind(&m); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -41,13 +42,72 @@ func (h *Handlers) PostAnswer(c *gin.Context) {
 		return
 	}
 	qidx := contest.GetQuestionIndex(m.QuestionID)
+	ca, err := helpers.StringToIntArray(contest.CorrectAnswersIndices, ",")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  err.Error(),
+			"status": http.StatusBadRequest,
+		})
+		return
+	}
+	correctAnswer := ca[qidx]
 	var track model.UserTrack
+	uinfo, err := r.GetUserInfo(uid)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  err.Error(),
+			"status": http.StatusBadRequest,
+		})
+		return
+	}
 	track.UserID = uid
+	track.ContestID = contest.ID
+	track.QuestionID = m.QuestionID
+	track.QuestionIndex = qidx
+
 	if qidx == 0 {
 		// it is the first question
+		if uinfo.CanPlay == false {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":  "Cannot answer this question",
+				"status": http.StatusPreconditionFailed,
+			})
+			return
+		}
+		if m.SelectedIndex == correctAnswer {
+			track.CanPlay = true
+			track.CanUseCorrector = true
+			track.IsSelectCorrectAnswer = true
+			track.State = model.PostAnswer
+		} else {
+			if contest.Meta.AllowedCorrectorUsageTimes > 0 && uinfo.Correctors >= contest.Meta.NeededCorrectors {
+				// reduce user correctors
+				uinfo.Correctors -= contest.Meta.NeededCorrectors
+				r.AddUserInfo(uinfo)
+				track.CanPlay = true
+				track.IsSelectCorrectAnswer = false
+				track.State = model.PostAnswer
+				if contest.Meta.AllowedCorrectorUsageTimes > 1 {
+					track.CanUseCorrector = true
+				} else {
+					track.CanUseCorrector = false
+				}
+			}
+		}
 	} else {
+		tracks, err := r.GetUserTracks(uid, m.ContestID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":  err.Error(),
+				"status": http.StatusBadRequest,
+			})
+			return
+		}
+		latestTrack := tracks[len(tracks)-1]
 
 	}
-	
-	
+
+}
+func returnError(err error, code int) {
+
 }
