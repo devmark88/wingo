1dd0e040a5fe0bf492bd5fb240dcb2ae625f5b71
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-17 03:12:44 +0330

    add userInfo publisher

---
+++ q/user_info.go
@@ -0,0 +1,21 @@
+package q
+
+import (
+	"github.com/RichardKnop/machinery/v1"
+	"gitlab.com/mt-api/wingo/model"
+	"gitlab.com/mt-api/wingo/response"
+)
+
+// PublishUserInfo : Publish User corrector and ticket
+func (q Question) PublishUserInfo(c model.UserInfo, srv *machinery.Server) error {
+	pub := Pub{Server: srv}
+	res := response.UserInfoPayload{}
+	res.Type = response.UserInfoPayloadEnum
+	res.Corrector = c.Correctors
+	res.ID = c.ID
+	res.Ticket = c.Tickets
+	res.CanPlay = c.CanPlay
+
+	e := pub.Publish(getUserTopic(c.ID), res)
+	return e
+}
