06495abcd8d21ede447298972bcd2bc57bf4b426
Mark Karamyar <mark.karamyar@gmail.com>
2019-02-17 12:01:51 +0330

    add referral, referrer, and new user handler imp

--- handlers/v1/shop.go
+++ handlers/v1/shop.go
@@ -32,12 +32,51 @@ func (h *Handlers) UpdateUserInfo(c *gin.Context) {
 	})
 }
 
-// AddReferral : add referral to the user
+// AddReferral : add referral to the referrer and referral user
 func (h *Handlers) AddReferral(c *gin.Context) {
+	var m request.ReferralRequest
 	r := repository.Connections{DB: h.Context.Connections.Database, Redis: h.Context.Connections.Cache, Queue: h.Context.Q.Server}
-	referralTicket := viper.GetInt("app.referralTicket")
-	referralCorrector := viper.GetInt("app.referralTicket")
-	code, err := updateUserInfo(h.Context.AuthUser.ID, uint(referralTicket), uint(referralCorrector), &r)
+	c.Header("Content-Type", "application/json; charset=utf-8")
+	if err := c.Bind(&m); err != nil {
+		attachErrorToContext(c, err, http.StatusBadRequest)
+		return
+	}
+	referralTicket := viper.GetInt("app.referral_tickets")
+	referralCorrector := viper.GetInt("app.referral_correctors")
+	refererTicket := viper.GetInt("app.referer_tickets")
+	refererCorrector := viper.GetInt("app.referer_correctors")
+	code, err := updateUserInfo(m.ReferralUserID, uint(referralTicket), uint(referralCorrector), &r)
+	if err != nil {
+		c.JSON(code, gin.H{
+			"error":  err.Error(),
+			"status": code,
+		})
+	}
+	code, err = updateUserInfo(m.ReferrerUserID, uint(refererTicket), uint(refererCorrector), &r)
+	if err != nil {
+		c.JSON(code, gin.H{
+			"error":  err.Error(),
+			"status": code,
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"error": nil,
+		"data":  true,
+	})
+}
+
+// NewUser : add ticket and corrector to new joiner
+func (h *Handlers) NewUser(c *gin.Context) {
+	var m request.NewUserRequest
+	r := repository.Connections{DB: h.Context.Connections.Database, Redis: h.Context.Connections.Cache, Queue: h.Context.Q.Server}
+	c.Header("Content-Type", "application/json; charset=utf-8")
+	if err := c.Bind(&m); err != nil {
+		attachErrorToContext(c, err, http.StatusBadRequest)
+		return
+	}
+	nt := viper.GetInt("app.new_user_tickets")
+	nc := viper.GetInt("app.new_user_correctors")
+	code, err := updateUserInfo(m.UserID, uint(nt), uint(nc), &r)
 	if err != nil {
 		c.JSON(code, gin.H{
 			"error":  err.Error(),
